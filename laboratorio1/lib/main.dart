df127f13f1c7f9242369ad2bcd12c75803914e93
Reina <reinasosa2024@gmail.com>
2026-09-13 15:45:35 -0600

    boton para restablecer todos los registros a su estado inicial

--- laboratorio1/lib/main.dart
+++ laboratorio1/lib/main.dart
@@ -1,5 +1,41 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const ListaAlumnosScreen(), 
+    );
+  }
+}
+
+//Laboratorio empieza aqui
 class Alumno {
   final String nombre;
   final String carnet;
@@ -25,11 +61,7 @@ class _ListaAlumnosScreenState extends State<ListaAlumnosScreen> {
     Alumno(nombre: 'Maria Lopez', carnet: '20210002', estaPresente: false),
     Alumno(nombre: 'Carlos Ramirez', carnet: '20210003', estaPresente: false),
     Alumno(nombre: 'Alvaro Días', carnet: '20212003', estaPresente: false),
-    Alumno(
-      nombre: 'Maria la del barrio',
-      carnet: '20240052',
-      estaPresente: false,
-    ),
+    Alumno(nombre: 'Maria la del barrio',carnet: '20240052', estaPresente: false),
     Alumno(nombre: 'Fin el humando', carnet: '20240003', estaPresente: false),
     Alumno(nombre: 'Jake el Perro', carnet: '20240201', estaPresente: false),
     Alumno(nombre: 'Felix Ave', carnet: '20220002', estaPresente: false),
@@ -66,141 +98,42 @@ class _ListaAlumnosScreenState extends State<ListaAlumnosScreen> {
         },
         padding: const EdgeInsets.only(bottom: 78),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          for (final alumno in alumnos) {
-            if (!alumno.estaPresente) {
-              setState(() {
-                alumno.estaPresente = true;
-              });
-            }
-          }
-        },
-        icon: Icon(Icons.check),
-        label: Text('Marcar todos como presentes'),
-      ),
-    );
-  }
-}
-
-// El Main desde aqui
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const ListaAlumnosScreen(), //MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: 300,
+            child: FloatingActionButton.extended(
+              onPressed: () {
+                setState(() {
+                  for (final alumno in alumnos) {
+                    if (!alumno.estaPresente) {
+                      alumno.estaPresente = true;
+                    }
+                  }
+                });
+              },
+              icon: const Icon(Icons.check),
+              label: const Text('Marcar a todo el grupo como presente'),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+
+          const SizedBox(height: 10),
+          SizedBox(
+            width: 300,
+            child: FloatingActionButton.extended(
+              onPressed: () {
+                setState(() {
+                  for (final alumno in alumnos) {
+                      alumno.estaPresente = false;
+                  }
+                });
+              },
+              icon: const Icon(Icons.refresh),
+              label: const Text('Restablecer todos los registros'),
+            ),
+          ),
+        ],
       ),
     );
   }
