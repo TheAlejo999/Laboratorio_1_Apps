2d1cac106739880a13b5c3af8de19515077c0f41
Kathleen Argueta <20245083@esen.edu.sv>
2026-09-13 18:15:31 -0600

    feat: implementar encabezado fijo sin desbordamientos y corregir reinicio del contador a 0

--- laboratorio1/lib/main.dart
+++ laboratorio1/lib/main.dart
@@ -11,10 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Control de Asistencia',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const ListaAlumnosScreen(), 
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const ListaAlumnosScreen(),
     );
   }
 }
@@ -35,21 +33,25 @@ class Alumno {
 
 // Datos de los alumnos
 List<Alumno> alumnos = [
-    Alumno(nombre: 'Juan Perez', carnet: '20210001', estadoInicial: true),
-    Alumno(nombre: 'Maria Lopez', carnet: '20210002', estadoInicial: false),
-    Alumno(nombre: 'Carlos Ramirez', carnet: '20210003', estadoInicial: false),
-    Alumno(nombre: 'Alvaro Días', carnet: '20212003', estadoInicial: false),
-    Alumno(nombre: 'Maria la del barrio',carnet: '20240052', estadoInicial: true),
-    Alumno(nombre: 'Fin el humando', carnet: '20240003', estadoInicial: false),
-    Alumno(nombre: 'Jake el Perro', carnet: '20240201', estadoInicial: false),
-    Alumno(nombre: 'Felix Ave', carnet: '20220002', estadoInicial: false),
-    Alumno(nombre: 'Antonio Molina', carnet: '20210034', estadoInicial: false),
-    Alumno(nombre: 'Mision Posible', carnet: '20200102', estadoInicial: false),
-    Alumno(nombre: 'Andre Chavez', carnet: '20190003', estadoInicial: false),
-    Alumno(nombre: 'Paco PocaSangre', carnet: '20211201', estadoInicial: false),
-    Alumno(nombre: 'Maria Antonieta', carnet: '20222202', estadoInicial: true),
-    Alumno(nombre: 'Pablo Escobar', carnet: '20211903', estadoInicial: false),
-    Alumno(nombre: 'Escubi Duh', carnet: '20251923', estadoInicial: false),
+  Alumno(nombre: 'Juan Perez', carnet: '20210001', estadoInicial: false),
+  Alumno(nombre: 'Maria Lopez', carnet: '20210002', estadoInicial: false),
+  Alumno(nombre: 'Carlos Ramirez', carnet: '20210003', estadoInicial: false),
+  Alumno(nombre: 'Alvaro Días', carnet: '20212003', estadoInicial: false),
+  Alumno(
+    nombre: 'Maria la del barrio',
+    carnet: '20240052',
+    estadoInicial: false,
+  ),
+  Alumno(nombre: 'Fin el humando', carnet: '20240003', estadoInicial: false),
+  Alumno(nombre: 'Jake el Perro', carnet: '20240201', estadoInicial: false),
+  Alumno(nombre: 'Felix Ave', carnet: '20220002', estadoInicial: false),
+  Alumno(nombre: 'Antonio Molina', carnet: '20210034', estadoInicial: false),
+  Alumno(nombre: 'Mision Posible', carnet: '20200102', estadoInicial: false),
+  Alumno(nombre: 'Andre Chavez', carnet: '20190003', estadoInicial: false),
+  Alumno(nombre: 'Paco PocaSangre', carnet: '20211201', estadoInicial: false),
+  Alumno(nombre: 'Maria Antonieta', carnet: '20222202', estadoInicial: false),
+  Alumno(nombre: 'Pablo Escobar', carnet: '20211903', estadoInicial: false),
+  Alumno(nombre: 'Escubi Duh', carnet: '20251923', estadoInicial: false),
 ];
 
 // Widget independiente para cada alumno
@@ -57,21 +59,14 @@ class AlumnoTile extends StatelessWidget {
   final Alumno alumno;
   final ValueChanged<bool?> onChanged;
 
-  const AlumnoTile({
-    super.key,
-    required this.alumno,
-    required this.onChanged,
-  });
+  const AlumnoTile({super.key, required this.alumno, required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(alumno.nombre),
       subtitle: Text('Carnet: ${alumno.carnet}'),
-      trailing: Checkbox(
-        value: alumno.estaPresente,
-        onChanged: onChanged,
-      ),
+      trailing: Checkbox(value: alumno.estaPresente, onChanged: onChanged),
     );
   }
 }
@@ -86,22 +81,57 @@ class ListaAlumnosScreen extends StatefulWidget {
 class _ListaAlumnosScreenState extends State<ListaAlumnosScreen> {
   @override
   Widget build(BuildContext context) {
+    final int presentes = alumnos.where((alumno) => alumno.estaPresente).length;
     return Scaffold(
-      appBar: AppBar(title: Text('Lista de Alumnos')),
-      body: ListView.builder(
-        itemCount: alumnos.length,
-        itemBuilder: (context, index) {
-          final alumno = alumnos[index];
-          return AlumnoTile(
-            alumno: alumno,
-            onChanged: (value) {
-              setState(() {
-                alumno.estaPresente = value ?? false;
-              });
-            },
-          );
-        },
-        padding: const EdgeInsets.only(bottom: 78),
+      appBar: AppBar(title: const Text('Lista de Alumnos')),
+      body: Column(
+        children: [
+          //Encabezado
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 12.0,
+            ),
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Flexible(
+                  child: Text(
+                    'Nosé.js',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  'Presentes: $presentes / ${alumnos.length}',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Lista de alumnos
+          Expanded(
+            child: ListView.builder(
+              itemCount: alumnos.length,
+              itemBuilder: (context, index) {
+                final alumno = alumnos[index];
+                return AlumnoTile(
+                  alumno: alumno,
+                  onChanged: (value) {
+                    setState(() {
+                      alumno.estaPresente = value ?? false;
+                    });
+                  },
+                );
+              },
+              padding: const EdgeInsets.only(bottom: 78),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Column(
         mainAxisSize: MainAxisSize.min,
@@ -130,7 +160,7 @@ class _ListaAlumnosScreenState extends State<ListaAlumnosScreen> {
               onPressed: () {
                 setState(() {
                   for (final alumno in alumnos) {
-                      alumno.estaPresente = alumno.estadoInicial;
+                    alumno.estaPresente = alumno.estadoInicial;
                   }
                 });
               },
